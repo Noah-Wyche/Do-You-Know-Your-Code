ab3db7185be3c3ecb75ad028c8611475418c25fb
Noah Wyche <noahwyche1@gmail.com>
2023-10-11 15:01:38 -0400

    First commit. added all the necessary files and much of my starting code. I created the html page and added the outline for the opening page of the webpage. I added the CSS page and added my initial styling. I also created the javaScript file and added notes outlinin the steps I will take to complete this project

---
+++ assets/javaScript/script.js
@@ -0,0 +1,17 @@
+// Ready for fresh Code!!
+
+// Here I will add an event listener for the click of the start button to begin the quiz
+
+// Here I will add a function to present the first question and start the timer when the quiz begins
+
+// Here I will add a function to present the second question after the first and so on
+
+// I also need it so that when a question is answered the timer either stays the same or time is removed depending on if the answer is correct or not
+
+// I need it to display the users score at the end of the quiz and provide a text box to enter the users initials and save them along with their score
+
+// I need to save those scores to local storage and have them accesible for later viewing by clicking a link
+
+// I need it to display "CORRECT" or "INCORRECT" at the bottom of the screen appropriately after each question is answered
+
+// If all the above is done I need to ensure I am not missing any steps and that all the code is written as effeciently as possible
